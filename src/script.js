e0d86ecb52ec29793700e7a09a9f13fd94d26e5b
Jason Kao <jason.kao@columbia.edu>
2019-10-04 22:39:06 -0400

    icon close svg and footer

--- src/script.js
+++ src/script.js
@@ -40,12 +40,12 @@ video.addEventListener('loadedmetadata', () => playbackRate = (deckOverlay.getBo
 let coverHeight = null;
 
 function scrollPlay() {
-  console.log(playbackRate)
   let frameNumber = window.pageYOffset;
   if (frameNumber < coverHeight) {
      frameNumber /= playbackRate;
-    if (frameNumber !== video.currentTime)
+    if (frameNumber !== video.currentTime && frameNumber <= video.duration) {
       video.currentTime = frameNumber;
+    }
   }
   window.requestAnimationFrame(scrollPlay);
 }
@@ -64,3 +64,11 @@ if (!USE_COVER_HED) {
 // Text balance headline and deck
 
 textBalancer.balanceText('.headline, .deck, .image-overlay .image-caption');
+
+/* SVG icon stuff
+
+const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10.29 12.3"><defs><style>.cls-1{fill:none;stroke:#999;stroke-miterlimit:10;}</style></defs><title>icon-close</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><line class="cls-1" x1="0.39" y1="0.32" x2="9.9" y2="11.99"/><line class="cls-1" x1="0.39" y1="11.99" x2="9.9" y2="0.32"/></g></g></svg>`;
+const svg64 = window.btoa(svg);
+console.log(`url('data:image/svg+xml;base64,${svg64}')`);
+
+*/
